87f1dd1195e4a54e17fa53bd53bd7468618d314f
JJ Merelo <jjmerelo@gmail.com>
2021-09-08 17:54:42 +0200

    Añade entradas para el nombre de fichero

--- iv-checks-on-source.pl
+++ iv-checks-on-source.pl
@@ -11,20 +11,31 @@ use lib "lib";
 
 use Utility;
 
+# Fase
+my $fase = @ARGV[0] =~ /-(\d+)/;
+
+
 # Previa
 my $student_repo = Git->repository ( Directory => "." );
 
 # Algunas variables
 my @repo_files = $student_repo->command("ls-files");
 
-# Objetivo 0
-doing( "🎯 Objetivo 0" );
-for my $f (qw( README.md .gitignore LICENSE )) {
-  if ( grep( $f, @repo_files) )  {
-    say all_good( "$f presente" );
-  } else {
-    say sorry( "Falta $f" );
-  }
-}
-end_group();
 
+objetivo_0( @repo_files);
+
+
+# Objetivos
+sub objetivo_0 {
+  my @repo_files = @_;
+  # Objetivo 0
+  doing( "🎯 Objetivo 0" );
+  for my $f (qw( README.md .gitignore LICENSE )) {
+    if ( grep( $f, @repo_files) )  {
+      say all_good( "$f presente" );
+    } else {
+      say sorry( "Falta $f" );
+    }
+  }
+  end_group();
+}
