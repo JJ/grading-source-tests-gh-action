777850bcc5168dc3d6eb9f5bb0eb51c60645e40b
JJ Merelo <jjmerelo@gmail.com>
2021-10-07 07:23:19 +0200

    Añade testeo fichero

--- dist/iv-checks-on-source.pl
+++ dist/iv-checks-on-source.pl
@@ -14026,7 +14026,7 @@ my @repo_files = $student_repo->command("ls-files");
 
 objetivo_0(@repo_files);
 
-exit if $fase <= 1;
+exit if $fase <= 2;
 
 # Fase 2
 my ($readme_file) = grep( /^README/, @repo_files );
@@ -14036,8 +14036,11 @@ my $iv;
 eval { $iv = LoadFile("iv.yaml"); };
 if ($@) {
   error( sorry( "Hay algún problema leyendo «iv.yaml» ⤷ $_" ) );
+} else {
+  say all_good( "Fichero de configuración ⚙ «iv.yaml» encontrado" );
 }
 
+objetivo_1( $iv, \@repo_files );
 
 
 # Objetivos
@@ -14053,4 +14056,40 @@ sub objetivo_0 {
     }
   }
   end_group();
+}
+
+sub objetivo_1 {
+  my $iv = shift;
+  for my $k (qw(lenguaje entidad)) {
+    comprueba( $iv->{$k},
+               "Clave $k está presente",
+               "Clave $k no está presente"
+             );
+  }
+
+  my $repo_files = shift;
+  file_present( $iv->{'entidad'}, $repo_files, "Con la entidad" );
+  end_group();
+}
+
+
+# Funciones de utilidad
+sub comprueba {
+  my ( $expresion, $bien, $mal ) = @_;
+  if ( $expresion ) {
+    say all_good($bien);
+  } else {
+    error( sorry( $mal ) );
+  }
+}
+
+sub file_present {
+  my ($file, $ls_files_ref, $name ) = @_;
+  my @files = (ref($file) eq 'ARRAY')?@$file:($file);
+  for my $file (@files ) {
+    comprueba( grep( /$file/, @$ls_files_ref ),
+               "Fichero $name → $file presente",
+               "Fichero $name → $file no está presente" );
+  }
+
 }
