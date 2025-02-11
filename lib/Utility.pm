42b761ac10e21a45626aedf6b52e78f4426fea10
JJ Merelo <jjmerelo@gmail.com>
2025-02-11 12:38:03 +0100

    #8 :construction:

--- lib/Utility.pm
+++ lib/Utility.pm
@@ -37,7 +37,7 @@ sub README_contiene_con_mensaje {
 }
 
 sub README_no_contiene_con_mensaje( $cadena, $README)  {
-  my @lineas = split("\n", $README );
+  say "README $README\n Cadena $cadena ";
   if ( index( $README, $cadena ) < 0 ) {
     say all_good("El README no contiene «$cadena»");
   } else {
