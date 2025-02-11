87ee3a1abe4920cc9862276763a9711364597319
JJ Merelo <jjmerelo@gmail.com>
2025-02-11 11:59:43 +0100

    #8 :sparkles:

--- lib/Utility.pm
+++ lib/Utility.pm
@@ -36,6 +36,22 @@ sub README_contiene_con_mensaje {
   }
 }
 
+sub README_no_contiene_con_mensaje( $cadena, $README)  {
+  my @lineas = split("\n", $README );
+  my @lineas_con_cadena = [];
+  for (my $l = 0; $l <= $#lineas; $l++ ) {
+    push @lineas_con_cadena, $l +1 if index($cadena, $lineas_con_cadena[$l] ) >= 0
+  }
+
+  if ( length( @lineas_con_cadena ) >= 1 ) {
+    for my $l (@lineas_con-cadena) {
+      error_on_file( sorry("El README no debe contener «$cadena»"), "README.md", $l );
+    }
+  } else {
+    say all_good("El README no contiene «$cadena»");
+  }
+}
+
 sub groupify( $wrapped_function, $group_name ) {
   return sub {
     doing( $group_name );
